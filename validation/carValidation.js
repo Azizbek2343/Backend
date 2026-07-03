e41674cf0df75555d7f095fdb609b5f953dddaf8
Azizbek2343 <nasritdinovAzizbek001@gmail.com>
2026-07-03 23:43:32 +0500

    Change

--- validation/carValidation.js
+++ validation/carValidation.js
@@ -3,21 +3,17 @@ const Joi = require("joi");
 const carValidationSchema = Joi.object({
     brand: Joi.string().trim().required(),
     model: Joi.string().trim().required(),
-    plate_number: Joi.string().required().min(3).max(6).pattern(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(_+{}\[\]:;<>,.?~\\/-]).{8,}$/
-    ),
-    color: Joi.string().trim,
-    passenger_capacity: Joi.string().min(2).max(8).required(),
+    plate_number: Joi.string().required().min(3).max(6),
+    color: Joi.string().trim(),
+    passenger_capacity: Joi.string().min(1).max(6).required(),
 });
 
 const updateCarValidationSchema = Joi.object({
     brand: Joi.string().trim().optional(),
     model: Joi.string().trim().optional(),
-    plate_number: Joi.string().min(3).max(6).pattern(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(_+{}\[\]:;<>,.?~\\/-]).{8,}$/
-    ),
-    color: Joi.string().trim().optional(),
-    passenger_capacity: Joi.string().min(2).max(8),
+    plate_number: Joi.string().min(3).max(6),
+    color: Joi.string().optional(),
+    passenger_capacity: Joi.string().min(1).max(6),
 });
 
 module.exports = {
